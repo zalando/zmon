51351f9ed94562db66a5e882a425fc2475cd1224
Henning Jacobs <henning@jacobs1.de>
2014-12-19 13:58:02 +0100

    fix API Schema creation

--- zmon-controller/database/zmon/20_api/00_create_schema.sql
+++ zmon-controller/database/zmon/20_api/00_create_schema.sql
@@ -1,2 +1,1 @@
-RESET ROLE;
-SELECT zz_utils.create_project_schema_api_env(schema_name:='zzm_api',data_schema_names:=ARRAY['zzm_data']);
+SELECT zz_utils.create_project_schema_api_env(schema_name:='zzm_api', data_schema_names:=ARRAY['zzm_data'], make_backward_compatible:=false);
