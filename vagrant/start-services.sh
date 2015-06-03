c107fb56ebf564791ff17909fe87968cae23e2d5
Jan Mussler <jan.mussler@zalando.de>
2015-06-03 14:46:32 +0200

    Bump versions for:\n fixing trial run auto complete\n Add Grafana Icon

--- vagrant/start-services.sh
+++ vagrant/start-services.sh
@@ -5,11 +5,14 @@ export PGUSER=postgres
 export PGPASSWORD=postgres
 export PGDATABASE=local_zmon_db
 
-export IMAGE_VERSION=0.1.4
+export EVENTLOG_VERSION=0.1.3
+export WORKER_VERSION=0.1.3
+export CONTROLLER_VERSION=0.1.6
+export SCHEDULER_VERSION=0.1.4
 
 if [ "b$1" = "b" ] || [ "b$1" = "beventlog-service" ] ; then
     docker rm zmon-eventlog-service
-    docker run --name zmon-eventlog-service --net host -e POSTGRESQL_USER=$PGUSER -e POSTGRESQL_PASSWORD=$PGPASSWORD -d os-registry.stups.zalan.do/stups/zmon-eventlog-service:$IMAGE_VERSION
+    docker run --name zmon-eventlog-service --net host -e POSTGRESQL_USER=$PGUSER -e POSTGRESQL_PASSWORD=$PGPASSWORD -d os-registry.stups.zalan.do/stups/zmon-eventlog-service:$EVENTLOG_VERSION
 
     until curl http://localhost:8081/\?key=alertId\&value=3\&types=212993 &> /dev/null; do
         echo 'Waiting for eventlog service'
@@ -20,7 +23,7 @@ fi
 if [ "b$1" = "b" ] || [ "b$1" = "bcontroller" ] ; then
    
     docker rm zmon-controller
-    docker run --name zmon-controller --net host -d os-registry.stups.zalan.do/stups/zmon-controller:$IMAGE_VERSION
+    docker run --name zmon-controller --net host -d os-registry.stups.zalan.do/stups/zmon-controller:$CONTROLLER_VERSION
 
     until curl http://localhost:8080/index.jsp &> /dev/null; do
         echo 'Waiting for ZMON Controller..'
@@ -28,9 +31,12 @@ if [ "b$1" = "b" ] || [ "b$1" = "bcontroller" ] ; then
     done
 fi
 
-for comp in scheduler-ng worker data-service; do
-    if [ "b$1" = "b" ] || [ "b$1" = "b$comp" ] ; then
-        docker rm zmon-$comp
-        docker run --name zmon-$comp --net host -d os-registry.stups.zalan.do/stups/zmon-$comp:$IMAGE_VERSION
-    fi
-done
+if [ "b$1" = "b" ] || [ "b$1" = "bworker" ] ; then
+    docker rm zmon-worker
+    docker run --name zmon-worker --net host -d os-registry.stups.zalan.do/stups/zmon-worker:$WORKER_VERSION
+fi
+
+if [ "b$1" = "b" ] || [ "b$1" = "bscheduler" ] ; then
+    docker rm zmon-scheduler
+    docker run --name zmon-scheduler --net host -d os-registry.stups.zalan.do/stups/zmon-scheduler-ng:$SCHEDULER_VERSION
+fi
