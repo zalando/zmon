2ca199e1cbebe71980edfa860447367818ab30e9
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-06-05 17:23:46 +0200

    save

--- js/canvas-setup.js
+++ js/canvas-setup.js
@@ -1,20 +1,32 @@
+// domready(function() {
+//     var stage = new createjs.Stage('hero-canvas'),
+//         circle = new createjs.Shape();
+//     circle.graphics.beginFill('Orange').drawCircle(0, 0, 50);
+//     circle.x = 100;
+//     circle.y = 100;
+//     stage.addChild(circle);
+//     stage.update();
+
+//     createjs.Tween
+//         .get(circle, { loop: true })
+//         .to({ scaleX: 2, scaleY: 2 }, 1000, createjs.Ease.getPowInOut(2))
+//         .to({ scaleX: 1, scaleY: 1}, 1000, createjs.Ease.getPowInOut(2));
+
+//     createjs.Ticker.setFPS(60);
+//     createjs.Ticker.addEventListener("tick", stage);
+// });
+// 
 domready(function() {
     var stage = new createjs.Stage('hero-canvas'),
-        circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-    stage.addChild(circle);
+        line = new createjs.Shape();
+    line
+        .graphics
+        .setStrokeStyle(1)
+        .beginStroke('Orange')
+        .moveTo(0, 100)
+        .lineTo(100, 0);
+    stage.addChild(line);
     stage.update();
-
-    createjs.Tween
-        .get(circle, { loop: true })
-        .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
-        .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
-        .to({ alpha: 0, y: 225 }, 100)
-        .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
-        .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
-
-    createjs.Ticker.setFPS(60);
-    createjs.Ticker.addEventListener("tick", stage);
-});
+    // stage.cache();
+    // stage.updateCache();
+})
