52b1f2035ef9c72db95f2459371210bd31f42d7b
Jan Mussler <jan.mussler@zalando.de>
2015-09-23 11:18:57 +0200

    Move to new registry location

--- vagrant/start.sh
+++ vagrant/start.sh
@@ -8,7 +8,7 @@ export PGDATABASE=local_zmon_db
 container=$(docker ps | grep postgres:9.4.0)
 if [ -z "$container" ]; then
     docker rm postgres
-    docker run --restart "on-failure:10" --name postgres --net host -e POSTGRES_PASSWORD=postgres -d os-registry.stups.zalan.do/stups/zmon-postgres:0.1.4
+    docker run --restart "on-failure:10" --name postgres --net host -e POSTGRES_PASSWORD=postgres -d registry.opensource.zalan.do/stups/zmon-postgres:0.1.4
 fi
 
 until nc -w 5 -z localhost 5432; do
@@ -30,7 +30,7 @@ psql -f /home/vagrant/zmon-eventlog-service/database/eventlog/00_create_schema.s
 container=$(docker ps | grep openldap)
 if [ -z "$container" ]; then
     docker rm openldap
-    docker run --restart "on-failure:10" --name openldap --net host -d os-registry.stups.zalan.do/stups/zmon-ldap:0.1.4
+    docker run --restart "on-failure:10" --name openldap --net host -d registry.opensource.zalan.do/stups/zmon-ldap:0.1.4
 fi
 
 until nc -w 5 -z localhost 389; do
@@ -43,7 +43,7 @@ ldapadd -c -x -D cn=admin,dc=example,dc=com -w toor -f /vagrant/vagrant/ldap-str
 container=$(docker ps | grep redis)
 if [ -z "$container" ]; then
     docker rm redis
-    docker run --restart "on-failure:10" --name redis --net host -d os-registry.stups.zalan.do/stups/zmon-redis:0.1.4
+    docker run --restart "on-failure:10" --name redis --net host -d registry.opensource.zalan.do/stups/zmon-redis:0.1.4
 fi
 
 until nc -w 5 -z localhost 6379; do
@@ -68,7 +68,7 @@ done
 container=$(docker ps | grep kairosdb)
 if [ -z "$container" ]; then
     docker rm kairosdb
-    docker run --restart "on-failure:10"  --name kairosdb --net host -d -e "CASSANDRA_HOST_LIST=$ip:9160" os-registry.stups.zalan.do/stups/zmon-kairosdb:0.1.6
+    docker run --restart "on-failure:10"  --name kairosdb --net host -d -e "CASSANDRA_HOST_LIST=$ip:9160" registry.opensource.zalan.do/stups/zmon-kairosdb:0.1.6
 fi
 
 until nc -w 5 -z localhost 8083; do
