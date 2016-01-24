ba4fe3976084353c2a1b67d62c8577a3469910b0
Bartosz Ocytko <bocytko@gmail.com>
2016-01-24 16:47:12 +0100

    Updated build commands to match state of current master branches of all zmon projects (mvnw, scm-source). Added error detection to the build commands.

--- build-env/build.sh
+++ build-env/build.sh
@@ -17,6 +17,14 @@ function progress {
     echo -e "${COLOR_PROGRESS}$1..${COLOR_RESET}"
 }
 
+function clone_or_pull {
+    if [ -d $1 ]; then
+        cd $1; git pull; cd ..
+    else
+        git clone https://github.com/zalando/$1.git
+    fi
+}
+
 echo "  ________  __  ____  _   _ "
 echo " |___  /  \/  |/ __ \| \ | |"
 echo "    / /| \  / | |  | |  \| |"
@@ -33,34 +41,35 @@ JAVA_VERSION=$(java -version 2>&1 | head -n 1 | grep -o '1\.[78]')
 PYTHON_VERSION=$(python --version 2>&1 | grep -o '2\.7')
 [ "v$PYTHON_VERSION" = "v2.7" ] || fail "Python 2.7 is required"
 
-cd /home/vagrant
+DIR=${2:-"/home/vagrant"}
+cd $DIR || fail "${DIR} does not exist"
 
 if [ "b$1" = "b" ] || [ "b$1" = "bcontroller" ] ; then
     progress 'Building Controller'
-    git clone https://github.com/zalando/zmon-controller.git
-    (cd zmon-controller && ./mvnw clean package && docker build -t zmon-controller .)
+    clone_or_pull zmon-controller
+    (cd zmon-controller && ./mvnw clean package && scm-source -f target/scm-source.json && docker build -t zmon-controller .) || fail "Error building Controller"
 fi
 
 if [ "b$1" = "b" ] || [ "b$1" = "beventlog-service" ] ; then
     progress 'Building Eventlog Service'
-    git clone https://github.com/zalando/zmon-eventlog-service.git
-    (cd zmon-eventlog-service && ./mvnw clean package && docker build -t zmon-eventlog-service .)
+    clone_or_pull zmon-eventlog-service
+    (cd zmon-eventlog-service && ./mvnw clean package && scm-source -f target/scm-source.json && docker build -t zmon-eventlog-service .) || fail "Error building Eventlog Service"
 fi
 
 if [ "b$1" = "b" ] || [ "b$1" = "bdata-service" ] ; then
     progress 'Building Data Service'
-    git clone https://github.com/zalando/zmon-data-service.git
-    (cd zmon-data-service && ./mvnw clean package && docker build -t zmon-data-service .)
+    clone_or_pull zmon-data-service
+    (cd zmon-data-service && ./mvnw clean package && docker build -t zmon-data-service .) || fail "Error building Data Service"
 fi
 
 if [ "b$1" = "b" ] || [ "b$1" = "bscheduler" ] ; then
     progress 'Building Scheduler'
-    git clone https://github.com/zalando/zmon-scheduler-ng.git zmon-scheduler
-    (cd zmon-scheduler && ./mvnw clean package && docker build -t zmon-scheduler .)
+    clone_or_pull zmon-scheduler
+    (cd zmon-scheduler && ./mvnw clean package && scm-source && docker build -t zmon-scheduler .) || fail "Error building Scheduler"
 fi
 
 if [ "b$1" = "b" ] || [ "b$1" = "bworker" ] ; then
     progress 'Building Worker'
-    git clone https://github.com/zalando/zmon-worker.git
-    (cd zmon-worker && docker build -t zmon-worker .)
+    clone_or_pull zmon-worker
+    (cd zmon-worker && scm-source && docker build -t zmon-worker .) || fail "Error building Worker"
 fi
