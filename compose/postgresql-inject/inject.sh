484f5c412dc7b287b8875cc6cb88de097fbcc688
Jan M <janm81@gmail.com>
2017-02-27 17:59:28 +0100

    Move to new controller with flyway. Added trust store to repo

--- compose/postgresql-inject/inject.sh
+++ compose/postgresql-inject/inject.sh
@@ -5,22 +5,7 @@ export PGUSER=postgres
 export PGPASSWORD=$POSTGRESQL_PASSWORD
 export PGDATABASE=local_zmon_db
 
-for i in zmon-controller zmon-eventlog-service; do
-    if [ -d /workdir/$i ]; then
-        rm -rf /workdir/$i
-    fi
-
-    wget https://github.com/zalando/$i/archive/master.zip -O /workdir/$i.zip
-    mkdir -p /workdir/$i
-    unzip /workdir/$i.zip -d /workdir/$i
-    rm /workdir/$i.zip
-done
-
-cd /workdir/zmon-controller/zmon-controller-master/database/zmon
-
 psql -c "CREATE DATABASE $PGDATABASE;" postgres
 psql -c 'CREATE EXTENSION IF NOT EXISTS hstore;'
 psql -c "CREATE ROLE zmon WITH LOGIN PASSWORD '--secret--';" postgres
-find -name '*.sql' | sort | xargs cat | psql
 
-psql -f /workdir/zmon-eventlog-service/zmon-eventlog-service-master/database/eventlog/00_create_schema.sql
